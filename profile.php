309287ea4d3573a0dce8653222ddb26fd64a74c4
Jr Facun <98105196+JrFacun@users.noreply.github.com>
2023-01-27 13:14:09 +0000

    Added Profile

---
+++ profile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="py-2">
+        <div class="container">
+            <div class="card-rounder-0">
+                <div class="card-body">
+                    <div class="w-100 justify-content-between d-flex">
+                        <h4><b>My Reservation</b></h4>
+                        <a href="./?p=edit_account" class="btn btn btn-dark btn-flat"><div class="fa fa-user-cog"></div>Manage Account </a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+</body>
+</html>
